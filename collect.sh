a0574ba592e845bfcb25a1ead1212a854e8cd433
du-xinyi <386476500@qq.com>
2025-06-05 19:51:12 +0800

    更新文档

--- collect.sh
+++ collect.sh
@@ -26,29 +26,6 @@ check_path "${workspace}/ARX_PLAY/mobile_aloha"
 
 check_executable "${workspace}/ARX_PLAY/realsense_camera/realsense.sh"
 
-if [ -d "${workspace}/LIFT" ] || [ -d "${workspace}/LIFT_ALL_IN_ONE" ]; then
-    target_dir=""
-    if [ -d "${workspace}/LIFT" ]; then
-        target_dir="${workspace}/LIFT"
-    else
-        target_dir="${workspace}/LIFT_ALL_IN_ONE"
-    fi
-
-    check_executable "${target_dir}/00-sh/ROS/remote_LIFT.sh"
-
-    gnome-terminal --title="lift" -- bash -c "cd ${target_dir}/00-sh/ROS/; bash remote_LIFT.sh; exec bash"
-    sleep 1
-elif [ -d "${workspace}/R5" ]; then
-    gnome-terminal --title="master" -- bash -c "cd ${workspace}/ARX_X5/ROS/X5_ws/; source ./devel/setup.bash && roslaunch arx_x5_controller open_remote_master.launch; exec bash"
-    sleep 1
-    gnome-terminal --title="slave" -- bash -c "cd ${workspace}/R5/ROS/R5_ws/; source ./devel/setup.bash && roslaunch arx_r5_controller open_remote_slave.launch; exec bash"
-    sleep 1
-else
-    echo "SDK not found" >&2
-
-    exit 1
-fi
-
 gnome-terminal --title="realsense" -- bash -c "cd ${workspace}/ARX_PLAY/realsense_camera; bash realsense.sh; exec bash"
 sleep 1
 gnome-terminal --title="collect" -- bash -c "cd ${workspace}/ARX_PLAY/mobile_aloha/; source ./venv/bin/activate; \
--- inference.sh
+++ inference.sh
@@ -26,38 +26,6 @@ check_path "${workspace}/ARX_PLAY/mobile_aloha"
 
 check_executable "${workspace}/ARX_PLAY/realsense_camera/realsense.sh"
 
-if [ -d "${workspace}/LIFT" ] || [ -d "${workspace}/LIFT_ALL_IN_ONE" ]; then
-    target_dir=""
-    if [ -d "${workspace}/LIFT" ]; then
-        target_dir="${workspace}/LIFT"
-    else
-        target_dir="${workspace}/LIFT_ALL_IN_ONE"
-    fi
-
-    check_path "${target_dir}/ARX_CAN/arx_can"
-    check_path "${target_dir}/body/ROS"
-    check_path "${target_dir}/LIFT_ARM/ROS/R5Pro_ws"
-
-    check_executable "${target_dir}/ARX_CAN/arx_can/arx_can1.sh"
-    check_executable "${target_dir}/ARX_CAN/arx_can/arx_can3.sh"
-    check_executable "${target_dir}/ARX_CAN/arx_can/arx_can5.sh"
-
-    gnome-terminal --title="lift" -- bash -c "cd ${target_dir}/body/ROS/; source ./devel/setup.bash && roslaunch arx_lift_controller lift.launch; exec bash"
-    sleep 1
-    gnome-terminal --title="r5pro" -- bash -c "cd ${target_dir}/LIFT_ARM/ROS/R5Pro_ws/; source ./devel/setup.bash && roslaunch arx_r5pro_controller open_double_arm.launch; exec bash"
-    sleep 1
-elif [ -d "${workspace}/R5" ]; then
-    check_executable "${workspace}/R5/ARX_CAN/arx_can/arx_can1.sh"
-    check_executable "${workspace}/R5/ARX_CAN/arx_can/arx_can3.sh"
-
-    gnome-terminal --title="r5" -- bash -c "cd ${workspace}/R5/ROS/R5_ws/; source ./devel/setup.bash && roslaunch arx_r5_controller open_double_arm.launch; exec bash"
-    sleep 1
-else
-    echo "SDK not found" >&2
-
-    exit 1
-fi
-
 gnome-terminal --title="realsense" -- bash -c "cd ${workspace}/ARX_PLAY/realsense_camera; bash realsense.sh; exec bash"
 sleep 1
 gnome-terminal --title="inference" -- bash -c "cd ${workspace}/ARX_PLAY/mobile_aloha; source ./venv/bin/activate; \
